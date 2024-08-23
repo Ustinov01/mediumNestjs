d7f96a58584fc3a67d4f71c0e39654b998a67266
Oleg <oleg.ustinov.1993@mail.ru>
2024-08-23 15:19:57 +0600

    login method done

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -8,16 +8,28 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/createUser.dto';
 import { userResponceInterface } from './types/userResponce.interface';
+import { LoginUserDto } from './dto/login.dto';
 
 @Controller()
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
   @Post('users')
   @UsePipes(new ValidationPipe())
   async createUser(
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<userResponceInterface> {
     const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserRespones(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async login(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<userResponceInterface> {
+    console.log(loginUserDto);
+    const user = await this.userService.login(loginUserDto);
     return this.userService.buildUserRespones(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,6 +6,8 @@ import { Repository } from 'typeorm';
 import { sign } from 'jsonwebtoken';
 import { JWT_SECRET } from '@app/config';
 import { userResponceInterface } from './types/userResponce.interface';
+import { LoginUserDto } from './dto/login.dto';
+import { compare } from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -32,6 +34,33 @@ export class UserService {
     Object.assign(newUser, createUserDto);
     console.log('newUser', newUser);
     return await this.userRepository.save(newUser);
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: { email: loginUserDto.email },
+      select: ['id', 'username', 'bio', 'email', 'image', 'password'],
+    });
+
+    if (!user) {
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    const correctPassword = await compare(loginUserDto.password, user.password);
+
+    if (!correctPassword) {
+      throw new HttpException(
+        'Credentials are not valid',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    delete user.password;
+
+    return user;
   }
 
   generateJwt(user: UserEntity): string {
